a14637ebb4e7f26d0d04530989ce98d7872fb492
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-11-13 02:05:38 +0300

    Add routes to frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,3 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-export default () => <div>Hi There!</div>;
+const SurveyNew = () => <h2>Suvery New</h2>;
+const Landing = () => <h2>Landing Page</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const Header = () => <h2>Header</h2>;
+
+export default () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route path="/" component={Landing} exact />
+        <Route path="/surveys" component={Dashboard} exact />
+        <Route path="/surveys/new" component={SurveyNew} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
