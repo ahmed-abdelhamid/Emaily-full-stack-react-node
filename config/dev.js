fd0748583c27d001ccb24e0194e26b79179aadde
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-11-15 10:06:48 +0300

    Add stripe keys

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '535704995505-u3t7k8g51b37lrhlgk1sh2tg7bq7ckg8.apps.googleusercontent.com',
-  googleClientSecret: '4g_5y0Horz5UgUoALjCTKaX2',
-  mongoURI:
-    'mongodb://ahmedsalah:Sallysaed1-@ds159293.mlab.com:59293/emaily-dev',
-  cookieKey: 'hjgjhjkkljlkjkldglkdlghsgdfdsgfgjhgjhgfgfhg'
-};
