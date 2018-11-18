ea3d000b10606df84e96faeafbeea73d9de5b16a
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-11-18 04:18:44 +0300

    Fix type error

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(passport.session());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
-  const path = requir('path');
+  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
